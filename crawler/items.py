b4fa6ef945af03f75ef921bfddafc916f18304c4
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-17 00:00:50 +0900

    Add CrawlerItem

--- crawler/items.py
+++ crawler/items.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author_level = scrapy.Field()
+    content = scrapy.Field()
+    uploaded_at = scrapy.Field()
+    view_count = scrapy.Field()
+    comment_count = scrapy.Field()
+    good_count = scrapy.Field()
+    bad_count = scrapy.Field()
+    
+
