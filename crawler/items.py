87de780dcd6214145b3b4b43f3c7dcebd0c766ed
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-21 00:04:25 +0900

    Change field
    작성자 레벨은 투자시간 대비 얻는
    효과가 적을 것 같아 제외했고,
    대신에 가볍게 스크랩이 가능한
    댓글에 대한 필드를 추가했다.

--- crawler/items.py
+++ crawler/items.py
@@ -4,12 +4,10 @@ import scrapy
 class CrawlerItem(scrapy.Item):
     page_no = scrapy.Field()
     title = scrapy.Field()
-    author_level = scrapy.Field()
     content = scrapy.Field()
+    comment = scrapy.Field()
     uploaded_at = scrapy.Field()
     view_count = scrapy.Field()
     comment_count = scrapy.Field()
     good_count = scrapy.Field()
     bad_count = scrapy.Field()
-    
-
