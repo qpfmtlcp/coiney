d808c409ada24bc142510d1026f50cae2a11e984
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-22 23:29:11 +0900

    Scrape comment data

--- crawler/loaders.py
+++ crawler/loaders.py
@@ -1,5 +1,5 @@
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst, MapCompose
+from scrapy.loader.processors import TakeFirst, MapCompose, Identity
 from w3lib.html import remove_tags
 
 
@@ -7,3 +7,5 @@ class CoinpanLoader(ItemLoader):
     default_output_processor = TakeFirst()
     
     content_in = MapCompose(remove_tags)
+    
+    comment_out = Identity()
--- crawler/spiders/coinpan.py
+++ crawler/spiders/coinpan.py
@@ -19,6 +19,7 @@ class CoinpanSpider(CrawlSpider):
         l.add_xpath('page_no', '//p[@class="perlink"]//@href', re='coinpan.com/(\d+)')
         l.add_xpath('title', '//div[@class="read_header"]//a/text()')
         l.add_xpath('content', '//div[@class="read_body"]//div[contains(@class, "xe_content")]')
+        l.add_xpath('comment', '//div[@id="comment"]//div[contains(@class, "xe_content")]/text()')
         
         header = l.nested_xpath('//ul[@class="wt_box gray_color"]//li')
         header.add_xpath('uploaded_at', '//span', re='\d{4}\.\d{2}\.\d{2}\W+\d{2}:\d{2}')
