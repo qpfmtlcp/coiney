489ffccd61dcc4119985fc0d8eabe49eeb308633
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-22 23:30:48 +0900

    Add login session
    코멘트 데이터를 얻기 위해
    로그인 기능이 필요했다.

--- crawler/spiders/coinpan.py
+++ crawler/spiders/coinpan.py
@@ -1,18 +1,27 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.http import FormRequest
 from crawler.loaders import CoinpanLoader
 from crawler.items import CrawlerItem
+import config
 
 
 class CoinpanSpider(CrawlSpider):
     name = 'coinpan'
     allowed_domains = ['coinpan.com']
-    start_urls = ['http://www.coinpan.com/index.php?mid=free&page=1']
+    start_urls = ['https://coinpan.com/index.php?mid=index&act=dispMemberLoginForm']
     
     rules = (
         Rule(LinkExtractor(allow=('/free$', 'index.php\?mid=free&page=\d+$'))),
         Rule(LinkExtractor(allow=('/free/\d+$', 'document_srl=\d+$',)), callback='parse_item'),
     )
+    
+    def parse_start_url(self, response):
+        return FormRequest.from_response(
+            response,
+            formdata={'user_id': config.ID, 'password': config.PASSWORD},
+            formxpath='//fieldset',
+        )
     
     def parse_item(self, response):
         l = CoinpanLoader(item=CrawlerItem(), response=response)
