16fd799cf2402411c39f1662a51141f213a80008
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-25 23:15:29 +0900

    Apply http cache

--- crawler/settings.py
+++ crawler/settings.py
@@ -66,19 +66,19 @@ SPIDER_MIDDLEWARES = {
 # }
 
 # Configure item pipelines
-# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'crawler.pipelines.DuplicatesPipeline': 300,
     'crawler.pipelines.ValidatesPipeline': 301,
 }
 
-# Enable and configure HTTP caching (disabled by default)
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = 'httpcache'
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# Configure HTTP caching
+HTTPCACHE_ENABLED = False
+HTTPCACHE_GZIP = True
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_POLICY = 'scrapy.extensions.httpcache.DummyPolicy'
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 # Configure log
 LOG_FORMATTER = 'crawler.middlewares.PoliteLogFormatter'
