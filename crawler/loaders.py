6d613a37d3696b37a679802a9349fa9742085c9b
jay <jay@jaemui-MacBook-Pro-2.local>
2017-11-27 20:52:03 +0900

    Fix content, comment cleansing
    문장별로 구분이 되어야 후처리나 분석 시
    한글 분석이 용이할 것으로 판단해
    해당 내용을 수정한다.

--- crawler/loaders.py
+++ crawler/loaders.py
@@ -3,7 +3,7 @@ from scrapy.loader.processors import TakeFirst, MapCompose, Identity
 from w3lib.html import remove_tags, replace_entities, replace_escape_chars
 
 
-def replace_useless_chars(text, which_ones=('', '\xa0'), replace_by=''):
+def replace_useless_chars(text, which_ones=('\xa0', '\t', '\r'), replace_by=''):
     for uc in which_ones:
         text = text.replace(uc, replace_by)
     return text
@@ -12,17 +12,17 @@ def replace_useless_chars(text, which_ones=('', '\xa0'), replace_by=''):
 class CoinpanLoader(ItemLoader):
     default_output_processor = TakeFirst()
     
-    content_in = MapCompose(remove_tags, replace_entities, replace_escape_chars, replace_useless_chars)
-    comment_in = MapCompose(remove_tags, replace_entities, replace_escape_chars, replace_useless_chars)
+    content_in = MapCompose(remove_tags, replace_entities, replace_useless_chars)
+    comment_in = MapCompose(remove_tags, replace_entities, replace_useless_chars)
     
     content_out = Identity()
-    comment_out = Identity()
+    comment_out = str
 
 
 class CoinGalleryLoader(ItemLoader):
     default_output_processor = TakeFirst()
     
-    content_in = MapCompose(remove_tags, replace_entities, replace_escape_chars, replace_useless_chars)
+    content_in = MapCompose(remove_tags, replace_entities, replace_useless_chars)
     view_count_in = MapCompose(replace_escape_chars, str.strip)
     
     content_out = Identity()
